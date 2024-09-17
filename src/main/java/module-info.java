a978e51905271268b5a4e9549957aa9d9ca4a233
Francesc Requesens <francesc.requesens@logicommerce.com>
2024-09-17 15:53:36 +0200

    Revert "feat: Update logicommerce-sdk and logicommerce-utilities versions"
    This reverts commit 9515d679a168f7a172d097694000733b78300eed.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.logicommerce.sdktest {
 	requires transitive com.logicommerce.sdk;
 	requires transitive java.logging;
-	requires transitive com.logicommerce.utilities;
+	requires com.logicommerce.utilities;
 	requires org.apache.httpcomponents.client5.httpclient5;
 	requires org.apache.httpcomponents.core5.httpcore5;
 
